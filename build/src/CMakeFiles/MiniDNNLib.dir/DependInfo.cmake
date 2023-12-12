de43bba5c095de6243cd368b7d6af33cc47c1269
VoLam <lamthanhvo3042002@gmail.com>
2023-12-12 10:20:08 +0700

    up file

---
+++ build/src/CMakeFiles/MiniDNNLib.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/layer/ave_pooling.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/ave_pooling.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/ave_pooling.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/layer/conv.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/conv.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/conv.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/layer/fully_connected.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/fully_connected.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/fully_connected.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/layer/max_pooling.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/max_pooling.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/max_pooling.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/layer/relu.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/relu.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/relu.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/layer/sigmoid.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/sigmoid.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/sigmoid.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/layer/softmax.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/softmax.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/softmax.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/loss/cross_entropy_loss.cc" "src/CMakeFiles/MiniDNNLib.dir/loss/cross_entropy_loss.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/loss/cross_entropy_loss.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/loss/mse_loss.cc" "src/CMakeFiles/MiniDNNLib.dir/loss/mse_loss.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/loss/mse_loss.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/mnist.cc" "src/CMakeFiles/MiniDNNLib.dir/mnist.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/mnist.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/network.cc" "src/CMakeFiles/MiniDNNLib.dir/network.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/network.cc.o.d"
+  "/Users/volam/Desktop/meterial/university/4th_education/lap_trinh_song_song/final_project/mini-dnn-cpp/src/optimizer/sgd.cc" "src/CMakeFiles/MiniDNNLib.dir/optimizer/sgd.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/optimizer/sgd.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
