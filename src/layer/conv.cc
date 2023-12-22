92b86591ebf4381c633644678864e5c422c7eaf8
VoLam <lamthanhvo3042002@gmail.com>
2023-12-22 17:59:32 +0700

    delete print

--- src/layer/conv.cc
+++ src/layer/conv.cc
@@ -72,14 +72,14 @@ void Conv::forward(const Matrix &bottom)
     const int K = height_kernel; // Assuming width_kernel is also K
 
     std::cout << "Conv-CPU==" << std::endl;
-    std::cout << *x << std::endl;
-    std::cout << *y << std::endl;
-    std::cout << *k << std::endl;
-    std::cout << *b << std::endl;
-    std::cout << B << std::endl;
-    std::cout << M << std::endl;
-    std::cout << C << std::endl;
-    std::cout << K << std::endl;
+    // std::cout << *x << std::endl;
+    // std::cout << *y << std::endl;
+    // std::cout << *k << std::endl;
+    // std::cout << *b << std::endl;
+    // std::cout << B << std::endl;
+    // std::cout << M << std::endl;
+    // std::cout << C << std::endl;
+    // std::cout << K << std::endl;
     for (int i = 0; i < n_sample; i++)
     {
         // im2col
