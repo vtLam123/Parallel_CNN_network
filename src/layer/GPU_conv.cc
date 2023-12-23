8456740137cf83bf4d9b077822f229f526687a03
VoLam <lamthanhvo3042002@gmail.com>
2023-12-23 16:19:56 +0700

    undo

--- src/layer/GPU_conv.cc
+++ src/layer/GPU_conv.cc
@@ -40,29 +40,29 @@ void Conv_Custom::forward(const Matrix &bottom)
     std::cout << "Conv-GPU==" << std::endl;
 
     // Launch marker kernel to aid with student function timing
-    GPU_Utils.insert_pre_barrier_kernel();
+    gpuUtils.insert_pre_barrier_kernel();
 
     // Start layer timer
     auto start_time_layer = std::chrono::high_resolution_clock::now();
     // Data transfer CPU to GPU
-    GPUInterface.conv_forward_gpu_prolog(y, x, k, &y_d, &x_d, &k_d, B, M, C, height_in, width_in, K);
+    gpuInterface.conv_forward_gpu_prolog(y, x, k, &y_d, &x_d, &k_d, B, M, C, height_in, width_in, K);
 
     // Start kernel timer
     auto start_time_kernel = std::chrono::high_resolution_clock::now();
     // Hand off to GPU for computation
-    GPUInterface.conv_forward_gpu(y_d, x_d, k_d, B, M, C, height_in, width_in, K);
+    gpuInterface.conv_forward_gpu(y_d, x_d, k_d, B, M, C, height_in, width_in, K);
     cudaDeviceSynchronize();
     // Stop kernel timer
     auto end_time_kernel = std::chrono::high_resolution_clock::now();
 
     // Data transfer GPU to CPU
-    GPUInterface.conv_forward_gpu_epilog(y, y_d, x_d, k_d, B, M, C, height_in, width_in, K);
+    gpuInterface.conv_forward_gpu_epilog(y, y_d, x_d, k_d, B, M, C, height_in, width_in, K);
 
     // Stop layer timer
     auto end_time_layer = std::chrono::high_resolution_clock::now();
 
     // Launch barrier kernel to aid with timing with nsight-compute
-    GPU_Utils.insert_post_barrier_kernel();
+    gpuUtils.insert_post_barrier_kernel();
 
     std::chrono::duration<float, std::milli> duration_layer = (end_time_layer - start_time_layer);
     std::cout << "Layer Time: " << duration_layer.count() << " ms" << std::endl;
