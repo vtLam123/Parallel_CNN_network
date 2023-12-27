4eb789d064f7639bde727fc5dc969d1322a98158
VoLam <lamthanhvo3042002@gmail.com>
2023-12-27 14:25:41 +0700

    add cuda frame

--- dnnNetwork.h
+++ dnnNetwork.h
@@ -17,6 +17,7 @@
 #include "src/layer/conv.h"
 #include "src/layer/CPU_conv.h"
 #include "src/layer/GPU_conv.h"
+#include "src/layer/my_GPU.h"
 #include "src/layer/fully_connected.h"
 #include "src/layer/ave_pooling.h"
 #include "src/layer/max_pooling.h"
