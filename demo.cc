ed34c3b5f955d5636c8f686a3fdd3d8c63914fa1
vmanh131 <macemi147@gmail.com>
2024-01-01 20:09:23 +0700

    change

--- demo.cc
+++ demo.cc
@@ -39,9 +39,9 @@ int main()
   std::cout << "mnist test number: " << dataset.test_labels.cols() << std::endl;
   // dnn
   Network dnn;
-  Layer *conv1 = new Conv_CPU(1, 28, 28, 4, 5, 5, 2, 2, 2);
+  Layer *conv1 = new Conv(1, 28, 28, 4, 5, 5, 2, 2, 2);
   Layer *pool1 = new MaxPooling(4, 14, 14, 2, 2, 2);
-  Layer *conv2 = new Conv_CPU(4, 7, 7, 16, 5, 5, 1, 2, 2);
+  Layer *conv2 = new Conv(4, 7, 7, 16, 5, 5, 1, 2, 2);
   Layer *pool2 = new MaxPooling(16, 7, 7, 2, 2, 2);
   Layer *fc3 = new FullyConnected(pool2->output_dim(), 32);
   Layer *fc4 = new FullyConnected(32, 10);
